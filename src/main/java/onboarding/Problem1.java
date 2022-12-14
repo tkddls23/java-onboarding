a2310090b41cf44090a5fa3198c70d3c79c710ea
wonseok <tkddls23@konkuk.ac.kr>
2022-12-14 23:12:59 +0900

    feat: 페이지의 범위가 올바른지 체크

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,6 +5,16 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+        if (!checkPageRange(pobi, crong)) {
+            return -1;
+        }
         return answer;
+    }
+
+    private static Boolean checkPageRange(List<Integer> pobi, List<Integer> crong) {
+        if (pobi.get(0) > 1 && pobi.get(1) < 400 && crong.get(0) > 1 && crong.get(1) < 400) {
+            return true;
+        }
+        return false;
     }
 }
