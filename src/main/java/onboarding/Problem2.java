82850c3dc3a2c8cfa2d1b53ca98b22aad74c4ad3
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 00:08:43 +0900

    feat: problem2 문자의 길이가 0인경우 체크

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -7,6 +7,9 @@ public class Problem2 {
     }
 
     private static boolean checkContinuousCryptogram(String cryptogram) {
+        if (cryptogram.length() == 0) {
+            return true;
+        }
         for(int i=0; i<cryptogram.length()-1; i++) {
             if(cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
                 return false;
