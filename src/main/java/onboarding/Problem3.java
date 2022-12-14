f3fd72933cc27a70f5c1fa6add432cd6484a7998
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 01:41:20 +0900

    feat: problem3 369 박수횟수 return

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,20 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            String numberString = Integer.toString(i);
+            answer += getNumberOfClap(numberString);
+        }
         return answer;
+    }
+
+    private static int getNumberOfClap(String numberString) {
+        int numberOfClap = 0;
+        for (int j = 0; j < numberString.length(); j++) {
+            if (numberString.charAt(j) == '3' || numberString.charAt(j) == '6' || numberString.charAt(j) == '9') {
+                numberOfClap++;
+            }
+        }
+        return numberOfClap;
     }
 }
