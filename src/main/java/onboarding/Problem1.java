f527d25353ecd1f0de676ebc07e18b3855e0468e
wonseok <tkddls23@konkuk.ac.kr>
2022-12-14 23:18:26 +0900

    feat: 페이지 번호가 연속적인지 체크

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,7 +5,7 @@ import java.util.List;
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
-        if (!checkPageRange(pobi, crong)) {
+        if (!checkPageRange(pobi, crong) || !checkPageSequence(pobi, crong)) {
             return -1;
         }
         return answer;
@@ -13,6 +13,13 @@ class Problem1 {
 
     private static Boolean checkPageRange(List<Integer> pobi, List<Integer> crong) {
         if (pobi.get(0) > 1 && pobi.get(1) < 400 && crong.get(0) > 1 && crong.get(1) < 400) {
+            return true;
+        }
+        return false;
+    }
+
+    private static Boolean checkPageSequence(List<Integer> pobi, List<Integer> crong) {
+        if ((pobi.get(1) - pobi.get(0)) == 1 && (crong.get(1) - crong.get(0)) == 1) {
             return true;
         }
         return false;
