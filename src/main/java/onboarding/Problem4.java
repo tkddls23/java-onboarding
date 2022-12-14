8ad380821cd3cdd3794e8e9e7bbf1c1cdb83a3a3
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 01:49:41 +0900

    feat: problem4 word 반대로 return

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,18 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        return getOppositeWord(word);
+    }
+    private static String getOppositeWord(String word) {
+        StringBuilder oppositeWord = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            if (!Character.isAlphabetic(word.charAt(i)))
+                oppositeWord.append(word.charAt(i));
+            if (Character.isUpperCase(word.charAt(i)))
+                oppositeWord.append((char) ('Z' - word.charAt(i) + 'A'));
+            if (Character.isLowerCase(word.charAt(i)))
+                oppositeWord.append((char)('z' - word.charAt(i) + 'a'));
+        }
+        return oppositeWord.toString();
     }
 }
