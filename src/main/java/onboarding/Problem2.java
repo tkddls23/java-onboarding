1fbfe512ca52db80d5dbbbe33125065fc43a9813
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 01:02:38 +0900

    feat: problem2 연속된 문자를 제거한다

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,8 +2,10 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        while (!checkContinuousCryptogram(cryptogram)) {
+            cryptogram = decrypt(cryptogram);
+        }
+        return cryptogram;
     }
 
     private static boolean checkContinuousCryptogram(String cryptogram) {
@@ -16,5 +18,36 @@ public class Problem2 {
             }
         }
         return true;
+    }
+
+    private static String decrypt(String cryptogram) {
+        int begin =getDuplicateIndexOfBegin(cryptogram);
+        int end =getDuplicateIndexOfEnd(cryptogram, begin);
+        if (begin != 0) {
+            return cryptogram.substring(0, begin) + cryptogram.substring(end);
+        }
+        return cryptogram.substring(end);
+    }
+
+    // 3. 중복일때 첫 인덱스 구하기
+    private static int getDuplicateIndexOfBegin(String cryptogram) {
+
+        for(int i = 0; i < cryptogram.length()-1 ; i++){
+            if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // 4. 중복일때 끝 인덱스 구하기
+    private static int getDuplicateIndexOfEnd(String cryptogram, int begin) {
+        int end;
+        for ( end = begin + 1 ; end < cryptogram.length(); end ++ ){
+            if ((cryptogram.charAt(begin) != cryptogram.charAt(end))) {
+                break;
+            }
+        }
+        return end;
     }
 }
