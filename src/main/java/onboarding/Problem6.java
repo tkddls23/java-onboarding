779410929c7e0b5ac48fdf63923b532eda131b70
wonseok <tkddls23@konkuk.ac.kr>
2022-12-15 02:43:38 +0900

    feat: problem6 중복 닉네임 정렬후 return

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,10 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        List<String> duplicatedList = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            for (int i = 0; i < nickname.length() - 1; i++) {
+                String word = nickname.substring(i, i + 2);
+                duplicatedList.addAll(findDuplicate(word, nickname, forms));
+            }
+        }
+        return duplicatedList.stream().distinct().sorted().collect(Collectors.toList());
+    }
+
+    private static List<String> findDuplicate(String word, String myNickname, List<List<String>> forms) {
+        List<String> duplicatedList = new ArrayList<>();
+
+        for (List<String> form : forms) {
+            String nickname = form.get(1);
+            String email = form.get(0);
+            if (nickname.equals(myNickname))
+                continue;
+            if (nickname.contains(word))
+                duplicatedList.add(email);
+        }
+        return duplicatedList;
     }
 }
